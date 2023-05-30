4c30851c081015aa7eab2615ec4aec5c9ecb6c11
Abraham Pech <abraham.opp@gmail.com>
2023-05-29 19:32:56 -0500

    Primer Maquetado

---
+++ index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="es-MX">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/styles.css">
+  <title>🚀 AnimeRocket</title>
+</head>
+
+<body>
+  <header>
+    <div class="header">
+      <div class="logo">🚀 AnimeRocket</div>
+      <nav>
+        <ul>
+          <li><a href="#">Inicio</a></li>
+          <li><a href="#">Directorio Anime</a></li>
+        </ul>
+        <div id="search">
+          <input type="text" placeholder="Buscar...">
+          <button>🔍</button>
+        </div>
+        <a href="#" class="login">🥷🏽 Login</a>
+      </nav>
+    </div>
+  </header>
+  <section id="main">
+    <h2>Últimos episodios</h2>
+    <div class="episodes">
+      <div class="episode">
+        <p class="icon">►</p>
+        <img src="https://picsum.photos/300/200" alt="">
+        <div class="episode_description">
+          <p class="episode_number">Episodio 1</p>
+          <h3 class="episode_title">Título del episodio</h3>
+        </div>
+      </div>
+      <div class="episode">
+        <p class="icon">►</p>
+        <img src="https://picsum.photos/300/200" alt="">
+        <div class="episode_description">
+          <p class="episode_number">Episodio 1</p>
+          <h3 class="episode_title">Título del episodio</h3>
+        </div>
+      </div>
+      <div class="episode">
+        <p class="icon">►</p>
+        <img src="https://picsum.photos/300/200" alt="">
+        <div class="episode_description">
+          <p class="episode_number">Episodio 1</p>
+          <h3 class="episode_title">Título del episodio</h3>
+        </div>
+      </div>
+      <div class="episode">
+        <p class="icon">►</p>
+        <img src="https://picsum.photos/300/200" alt="">
+        <div class="episode_description">
+          <p class="episode_number">Episodio 1</p>
+          <h3 class="episode_title">Título del episodio</h3>
+        </div>
+      </div>
+      <div class="episode">
+        <p class="icon">►</p>
+        <img src="https://picsum.photos/300/200" alt="">
+        <div class="episode_description">
+          <p class="episode_number">Episodio 1</p>
+          <h3 class="episode_title">Título del episodio</h3>
+        </div>
+      </div>
+      <div class="episode">
+        <p class="icon">►</p>
+        <img src="https://picsum.photos/300/200" alt="">
+        <div class="episode_description">
+          <p class="episode_number">Episodio 1</p>
+          <h3 class="episode_title">Título del episodio</h3>
+        </div>
+      </div>
+      <div class="episode">
+        <p class="icon">►</p>
+        <img src="https://picsum.photos/300/200" alt="">
+        <div class="episode_description">
+          <p class="episode_number">Episodio 1</p>
+          <h3 class="episode_title">Título del episodio</h3>
+        </div>
+      </div>
+      <div class="episode">
+        <p class="icon">►</p>
+        <img src="https://picsum.photos/300/200" alt="">
+        <div class="episode_description">
+          <p class="episode_number">Episodio 1</p>
+          <h3 class="episode_title">Título del episodio</h3>
+        </div>
+      </div>
+      <div class="episode">
+        <p class="icon">►</p>
+        <img src="https://picsum.photos/300/200" alt="">
+        <div class="episode_description">
+          <p class="episode_number">Episodio 1</p>
+          <h3 class="episode_title">Título del episodio</h3>
+        </div>
+      </div>
+      <div class="episode">
+        <p class="icon">►</p>
+        <img src="https://picsum.photos/300/200" alt="">
+        <div class="episode_description">
+          <p class="episode_number">Episodio 1</p>
+          <h3 class="episode_title">Título del episodio</h3>
+        </div>
+      </div>
+      <div class="episode">
+        <p class="icon">►</p>
+        <img src="https://picsum.photos/300/200" alt="">
+        <div class="episode_description">
+          <p class="episode_number">Episodio 1</p>
+          <h3 class="episode_title">Título del episodio</h3>
+        </div>
+      </div>
+      <div class="episode">
+        <p class="icon">►</p>
+        <img src="https://picsum.photos/300/200" alt="">
+        <div class="episode_description">
+          <p class="episode_number">Episodio 1</p>
+          <h3 class="episode_title">Título del episodio</h3>
+        </div>
+      </div>
+    </div>
+  </section>
+</body>
+
+</html>
